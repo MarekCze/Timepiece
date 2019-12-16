84fdaf259d9b98cdc5e4f2e54fdaf4cf49c5e982
MarekCze <marekcze@outlook.com>
2019-12-16 10:52:21 +0000

    got v for to display data

--- Client/src/components/Product.vue
+++ Client/src/components/Product.vue
@@ -1,52 +1,46 @@
 <template>
     <div class="wrapper">
-      <div class="product" v-for="(product, index) in products.product" :key="index">
+      <div class="product" v-for="(product, index) in products" :key="index">
         <div class="product-name">
           <h1>{{product.brand}}</h1>
           <h2>{{product.model}}</h2>
         </div>
-        <div class="product-image">
-          <img v-bind:src="'@/assets/img/' + getImage(product)" />
-        </div>
-        <div class="product-info">
-          <div class="color-box"
-            v-for="(v, index) in product.variant"
-            :key="v.variantId"
-            :style="{ backgroundColor: v.color }"
-            @mouseover="updateProduct(index)">
-          </div>
-        </div>
+          <img :src="getImage(product.variant[0].variantImage)">
       </div>
     </div>
 </template>
 <script>
 import ProductService from '@/services/ProductService'
 export default {
-  name: 'products',
+  name: 'product',
+  props: [
+    'brand'
+  ],
   data () {
     return {
-      products: []
-    }
-  },
-  mounted () {
-    this.getProducts()
-  },
-  computed: {
-    displayProducts: function () {
-      return this.products
+      products: [],
+      shoppingItems: [
+        { name: 'apple', price: '7' },
+        { name: 'orange', price: '12' }
+      ]
     }
   },
   methods: {
-    async getProducts () {
-      const response = await ProductService.fetchProducts()
-      this.products = response.data
+    async getProductsByBrand (brand) {
+      let response = await ProductService.fetchProductsByBrand(brand)
+      this.products = response.data.product
+      return this.products
+    },
+    check () {
+      console.log('products array is not empty')
     },
     updateProduct (index) {
       this.SelectedVariant = index
     },
-    getImage (product) {
-      console.log('assets/img/' + product.variant[product.SelectedVariant].variantImage)
-      return product.variant[product.SelectedVariant].variantImage
+    async getImage (image) {
+      let img = await ProductService.fetchImage(image)
+      console.log(img)
+      return img
     },
     showProducts () {
       let product = this.products.product
@@ -58,6 +52,9 @@ export default {
         return product
       }
     }
+  },
+  created () {
+    this.products = this.getProductsByBrand(this.brand)
   }
 }
 </script>
--- Server/index.js
+++ Server/index.js
@@ -19,8 +19,9 @@ const credentials = require('./credentials');
 
 
 const options = {
+    useNewUrlParser: true,
     server: {
-       socketOptions: { keepAlive: 1 } 
+       socketOptions: { keepAlive: 1 }
     }
 };
 
@@ -35,232 +36,6 @@ db.once("open", function(callback){
 const Watch = require('./models/watches');
 const Product = require('./models/Product');
 
-Product.find(function(err, products){
-    if(products.length) return;
-
-    new Product({
-        brand: 'Vincero',
-        model: 'CHRONO S-CLASS',
-        productId: 0,
-        SelectedVariant: 0,
-        description: String,
-        price: 229,
-        variant: [
-            {
-                variantId: 1,
-                color: 'blue',
-                variantImage: 'Chrono-S-Blue_Brown.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'black',
-                variantImage: 'Chrono-S-Matte-Black.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 3,
-                color: 'gold',
-                variantImage: 'Chrono-S-Black_Silver.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 4,
-                color: 'gold',
-                variantImage: 'Chrono-S-Rose-Gold.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-
-    new Product({
-        brand: 'Vincero',
-        model: 'KAIROS SERIES',
-        productId: 1,
-        SelectedVariant: 0,
-        description: String,
-        price: 189,
-        variant: [
-            {
-                variantId: 1,
-                color: 'blue',
-                variantImage: 'Kairos-Black_Gold-Mesh.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'black',
-                variantImage: 'Kairos-Matte-Black-Mesh.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 3,
-                color: 'black',
-                variantImage: 'Kairos-Blue_Brown.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 4,
-                color: 'black',
-                variantImage: 'Kairos-White_Silver.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-
-    new Product({
-        brand: 'Vincero',
-        model: 'BELLWETHER',
-        productId: 2,
-        SelectedVariant: 0,
-        description: String,
-        price: 280,
-        variant: [
-            {
-                variantId: 1,
-                color: 'green',
-                variantImage: 'Bellwether-Rose-Gold_Blue.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'blue',
-                variantImage: 'Bellwether-Rose-Gold_Black.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 3,
-                color: 'blue',
-                variantImage: 'Bellwether-Silver_White.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 4,
-                color: 'blue',
-                variantImage: 'Bellwether-Matte-Black.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-
-    new Product({
-        brand: 'Breguet',
-        model: 'CLASSIQUE',
-        productId: 3,
-        SelectedVariant: 0,
-        description: String,
-        price: 12000,
-        variant: [
-            {
-                variantId: 1,
-                color: 'darkblue',
-                variantImage: 'Breguet-Classique-Granite.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'black',
-                variantImage: 'Breguet-Classique-Black.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 3,
-                color: 'gold',
-                variantImage: 'Breguet-Classique-Gold.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-
-    new Product({
-        brand: 'Breguet',
-        model: 'MARINE',
-        productId: 4,
-        SelectedVariant: 0,
-        description: String,
-        price: 15000,
-        variant: [
-            {
-                variantId: 1,
-                color: 'salmon',
-                variantImage: 'Breguet-Marine-Black_Copper.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'black',
-                variantImage: 'Breguet-Marine-Black.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 3,
-                color: 'blue',
-                variantImage: 'Breguet-Marine-Blue.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-
-    new Product({
-        brand: 'Breguet',
-        model: 'HERITAGE',
-        productId: 5,
-        SelectedVariant: 0,
-        description: String,
-        price: 28000,
-        variant: [
-            {
-                variantId: 1,
-                color: 'green',
-                variantImage: 'Breguet-Heritage-Silver.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2,
-                color: 'blue',
-                variantImage: 'Breguet-Heritage-Copper.jpg',
-                variantQuantity: 10
-            }
-        ]
-    }).save();
-})
-
-Watch.find(function(err, watches){
-    if(watches.length) return;
-
-    new Watch({
-        brand: 'Vincero',
-        model: 'Chrono',
-        variant: 'Black',
-        price: 230,
-        description: 'description',
-        available: true,
-        numInStock: 80,
-        image: 'vinceroChronoBlack.jpg'
-    }).save();
-
-    new Watch({
-        brand: 'Vincero',
-        model: 'Chrono',
-        variant: 'Gold',
-        price: 210,
-        description: 'description',
-        available: true,
-        numInStock: 30,
-        image: 'vinceroChronoGold.jpg'
-    }).save();
-
-    new Watch({
-        brand: 'Vincero',
-        model: 'Kairos',
-        variant: 'Black',
-        price: 189,
-        description: 'description',
-        available: true,
-        numInStock: 200,
-        image: 'vinceroKairosBlack.jpg'
-    }).save();
-});
 
 app.get('/', function(req, res) {
 	res.json('home');
@@ -293,6 +68,27 @@ app.get('/products', (req, res) => {
         }
     })       
 });
+
+app.get('/products/:brand', (req, res) => {
+    Product.find({brand: req.params.brand}, function (err, products) {       
+        try{
+            res.json({
+                product: products
+            });
+        } catch (err){
+            console.log(error)
+            return res.json('500')
+        }
+    })       
+});
+
+app.get('/products/:image', (req,res) => {
+    try{
+        res.sendFile(`/assets/img/${req.params.image}`)
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
